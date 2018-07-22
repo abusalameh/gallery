d6f475a5d68b8ceac0e115b6dcf552f699dfe195
Raja Abu Salameh <future.adv2@gmail.com>
2018-07-22 18:45:44 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Panels/>
+  </div>
+</template>
+
+<script>
+import Panels from './components/Panels'
+
+export default {
+  name: 'App',
+  components: {
+    Panels
+  }
+}
+</script>
+
+<style>
+html {
+      box-sizing: border-box;
+      background:#ffc600;
+      font-family:'helvetica neue';
+      font-size: 20px;
+      font-weight: 200;
+    }
+    body {
+      margin: 0;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+</style>
